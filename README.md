2c503ae6e5134d2731b00bd590801ed8a16a1c44
Aom Siriwat <siriwat576@gmail.com>
2023-03-04 17:18:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kotlin-GuessTheNumber
+ (Kotlin) Guess the Number App
---
+++ app/src/main/java/com/aom/guessthenumber/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.aom.guessthenumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    var challengeNumber: Int = 0
+    var scoreNumber: Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // หา Button โดยใช้ ID ชื่อว่า "guessNumberButton"
+        val guessNumberButton = findViewById<Button>(R.id.guessNumberButton)
+        // หา Button โดยใช้ ID ชื่อว่า "newGameButton"
+        val newGameButton = findViewById<Button>(R.id.newGameButton)
+
+        // ตั้ง Event onClick
+        guessNumberButton.setOnClickListener {
+            // Action ตอนกดปุ่ม "Guess the Number!
+            onGuessing()
+        }
+        newGameButton.setOnClickListener {
+            newGame()
+        }
+        toggleHintLayout(false)
+        newGame()
+    }
+
+    fun toggleHintLayout(toggle: Boolean) {
+        var hintLayout = findViewById<LinearLayout>(R.id.hintLayout)
+        if(toggle)
+            hintLayout.visibility = View.VISIBLE
+        else
+            hintLayout.visibility = View.INVISIBLE
+    }
+
+    fun onGuessing() {
+        // หา Input โดยใช้ ID ชื่อว่า "guessNumberInput"
+        var guessNumberInput = findViewById<EditText>(R.id.guessNumberInput)
+        // หา Text โดยใช้ ID ชื่อว่า "hintValueText"
+        var hintText = findViewById<TextView>(R.id.hintValueText)
+        // รับค่าจาก guessNumberInput และแปลงเป็นตัวเลขเพื่อจะนำมาเปรียบเทียบ
+        val guessNumber = guessNumberInput.getText().toString()
+
+        if(guessNumber.isEmpty()) return newGame()
+        toggleHintLayout(true)
+        if(guessNumber.toInt() < challengeNumber)
+            hintText.setText("Lower" + challengeNumber.toString())
+        else if(guessNumber.toInt() > challengeNumber)
+            hintText.setText("Higher" + challengeNumber.toString())
+        else
+            correct()
+    }
+
+    fun newGame(resetScore: Boolean = true) {
+        var guessNumberInput = findViewById<EditText>(R.id.guessNumberInput)
+        toggleHintLayout(false)
+        if(resetScore) updateScore(0)
+        challengeNumber = (0 until 1000).random()
+        guessNumberInput.getText().clear()
+    }
+
+    fun correct() {
+        updateScore(scoreNumber+1)
+        Toast.makeText(applicationContext,"YOU WIN!",Toast.LENGTH_SHORT).show()
+        toggleHintLayout(false)
+        newGame(false)
+    }
+
+    fun updateScore(value: Int) {
+        scoreNumber = value
+        var scoreText = findViewById<TextView>(R.id.scoreValue)
+        scoreText.setText(scoreNumber.toString())
+    }
+}
